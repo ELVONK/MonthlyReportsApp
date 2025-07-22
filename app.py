a7bffd20debafdc819e8049e85d67e2a17669cff
Elvis Kipng'eno <kemelvis@gmail.com>
2025-07-22 14:09:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from report_dashboard import report_dashboard
 from daily_form import daily_work_form
 
 st.set_page_config(page_title="KURA Reporting System", layout="wide")
@@ -6,7 +7,7 @@ st.set_page_config(page_title="KURA Reporting System", layout="wide")
 menu = st.sidebar.selectbox("Navigation", ["Reports", "Daily Works"])
 
 if menu == "Reports":
-    st.title("📊 Monthly Report Dashboard")
-    st.info("Report module coming soon...")
+    report_dashboard()
 elif menu == "Daily Works":
     daily_work_form()
+
