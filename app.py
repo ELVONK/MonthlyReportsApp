a41cdcf12b2323094cf98d739ac0a0eed9ea0374
Elvis Kipng'eno <kemelvis@gmail.com>
2025-07-22 14:47:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from report_dashboard import report_dashboard
 from daily_form import daily_work_form
+from report_dashboard import report_dashboard
 
 st.set_page_config(page_title="KURA Reporting System", layout="wide")
 
